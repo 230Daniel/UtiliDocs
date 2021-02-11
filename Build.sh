2a4e77f47d10b3dc3d3d589f256a7a449278e642
Daniel Baynton <49287178+230Daniel@users.noreply.github.com>
2021-02-11 10:42:55 +0000

    Add files via upload

---
+++ Build.sh
@@ -0,0 +1,2 @@
+screen -S u2-docs-build -X quit
+screen -dmS u2-docs-build yarn build ; ./Start.sh
---
+++ Start.sh
@@ -0,0 +1,3 @@
+cd /home/UtiliDocs/build
+screen -S u2-docs -X quit
+screen -dmS u2-docs yarn serve
