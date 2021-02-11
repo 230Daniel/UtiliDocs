a1929b4518a456e782cfca19fe909d804178dd4d
Daniel Baynton <daniel.baynton@hotmail.com>
2021-02-11 11:18:34 +0000

    Remove home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/230Daniel/UtiliDocs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
