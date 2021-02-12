8ec17b4038d2439234f66f58f3773dd58d646874
Daniel Baynton <daniel.baynton@hotmail.com>
2021-02-12 11:26:07 +0000

    Test adding page

---
+++ docs/autopurge.md
@@ -0,0 +1,7 @@
+---
+id: Autopurge
+title: Autopurge
+sidebar_label: Autopurge
+---
+
+Hello this is the autopurge page
--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,14 @@
 module.exports = {
   docs: [
     {
-      type: 'category',
-      label: 'Getting Started',
-      items: ['Welcome'],
-    }
+        type: 'category',
+        label: 'Getting Started',
+        items: ['Welcome'],
+    },
+      {
+          type: 'category',
+          label: 'Features',
+          items: ['Autopurge']
+      }
   ],
 };
